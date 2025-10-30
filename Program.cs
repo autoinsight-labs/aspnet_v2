fe1ba27f2043e28e307778f4e8139f657ce15198
Arthur Mariano <arthvm@proton.me>
2025-10-30 14:32:11 -0300

    feat(api-docs): add OpenAPI and Scalar support
    This adds OpenAPI generation and the Scalar API reference UI to the project. The documentation is only available in the development environment.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 using AutoInsight.Data;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(
@@ -9,5 +11,11 @@ builder.Services.AddDbContext<AppDbContext>(options =>
         .UseSnakeCaseNamingConvention());
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
 
 app.Run();
